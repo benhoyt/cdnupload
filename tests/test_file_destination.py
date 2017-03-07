1b5e63f7237cc0be9127da59c42de52a61ce7a68
Ben Hoyt <bhoyt@bhoyt-MBP.local>
2017-03-07 17:46:33 -0500

    Add FileDestination tests

--- tests/test_file_destination.py
+++ tests/test_file_destination.py
@@ -1,13 +1,37 @@
 """Test FileDestination class."""
 
-from cdnupload import FileDestination
+from cdnupload import FileDestination, FileSource
 
 
-def test_file_destination(tmpdir):
-    d = FileDestination(tmpdir.strpath)
+def test_str():
+    d = FileDestination('foo/bar')
+    assert str(d) == 'foo/bar'
+
+
+def test_file_operations(tmpdir):
+    tmpdir.join('src').mkdir()
+    tmpdir.join('src', 'file.txt').write_binary(b'foo')
+    tmpdir.join('src', 'subdir').mkdir()
+    tmpdir.join('src', 'subdir', 'subfile.txt').write_binary(b'foo')
+
+    s = FileSource(tmpdir.join('src').strpath)
+    keys = s.build_key_map()
+    assert keys == {
+        'file.txt': 'file_0beec7b5ea3f0fdb.txt',
+        'subdir/subfile.txt': 'subdir/subfile_0beec7b5ea3f0fdb.txt',
+    }
+
+    d = FileDestination(tmpdir.join('dest').strpath)
     assert sorted(d.keys()) == []
 
-    tmpdir.join('file.txt').write_binary(b'foo')
-    tmpdir.join('dir').mkdir()
-    tmpdir.join('dir', 'dirfile.txt').write_binary(b'bar')
-#    assert sorted(d.keys()) == []
+    d.upload(keys['file.txt'], s, 'file.txt')
+    assert sorted(d.keys()) == ['file_0beec7b5ea3f0fdb.txt']
+
+    d.upload(keys['subdir/subfile.txt'], s, 'subdir/subfile.txt')
+    assert sorted(d.keys()) == [
+        'file_0beec7b5ea3f0fdb.txt',
+        'subdir/subfile_0beec7b5ea3f0fdb.txt',
+    ]
+
+    d.delete(keys['file.txt'])
+    assert sorted(d.keys()) == ['subdir/subfile_0beec7b5ea3f0fdb.txt']
